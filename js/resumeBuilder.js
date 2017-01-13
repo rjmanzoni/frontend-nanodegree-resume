6196142e6d298b67b694f08873fd0975dd9384b2
rmanzoni <rmanzoni@uolinc.com>
2017-01-13 20:56:54 -0200

    asdf

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var myName = "Renato Manzoni";
+var myRole = "Software Engineering";
+var name = HTMLheaderName.replace("%data%", myName);
+var role = HTMLheaderRole.replace("%data%", myRole);
+
+$("#header").append(name);
+$("#header").append(role);
