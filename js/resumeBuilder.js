9c50cf38295765e12acba17b9725e00ea40a4d8a
Renato de Jesus Manzoni <rjmanzoni@gmail.com>
2017-01-16 00:36:13 -0200

    resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,7 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 var bio = {
-    "name" : "Renato de Jesus Manzoni",
+    "name" : "Renato Manzoni",
     "role" : "Software Engineering",
     "contacts" : {
         "mobile" : "+55 11 98356 3656",
@@ -41,26 +41,99 @@ var education = {
 var work = {
     "jobs" : [
         {
-            "employer" : "UNIVERSO ONLINE - UOL",
+            "employer" : "UNIVERSO ONLINE",
             "title" : "Software Engineer",
-            "projects" : [
-                {
-                    "name" : "User Authentication and Authorization",
-                    "description" : "Java-based system development and maintenance for authentication and authorization of users (Single sign-on). The system is designed to support thousands of requests per second, horizontally scalability and ensure the safety of users' passwords."
-                },
-                {
-                    "name" : "RESTful Web Service Authentication and Authorization",
-                    "description" : "Client-server authentication protocol development for REST services. This protocol is based on Amazon Rest Authentication and HTTP Digest Authentication. A software architecture was designed to manage the credentials of clients and servers in order to facilitate integration."
-                },
-                {
-                    "name": "Device Fingerprint",
-                    "description" : "Java-based systems development and maintenance to identify devices according to the information provided by its Browser. This information is used to recognize potential fraud, finding similar devices and classifing them according to their profile."
-                },
-                {
-                    "name": "Phishing Notification",
-                    "description" : "Java-based systems development and maintenance that identifies phishing based on the web site domain. The notification is done by a JavaScript that sends a request for servers stating the origin of phishing."
-                }
-            ]
+            "description" : "Software Engineer working in Java backend systems development with a focus on scalability, performance, interoperability and security.",
+            "dates" : "2011 - Present",
+            "location" : "São Paulo - SP, Brazil",
         }
     ]
 }
+
+var projects = {
+    "projects" : [
+                {
+                    "title" : "User Authentication and Authorization",
+                    "dates" : "2011 - 2013",
+                    "description" : "Java-based system development and maintenance for authentication and authorization of users (Single sign-on). The system is designed to support thousands of requests per second, horizontally scalability and ensure the safety of users' passwords."
+                },
+                {
+                    "title" : "RESTful Web Service Authentication and Authorization",
+                    "dates" : "2011 - 2012",
+                    "description" : "Client-server authentication protocol development for REST services. This protocol is based on Amazon Rest Authentication and HTTP Digest Authentication. A software architecture was designed to manage the credentials of clients and servers in order to facilitate integration."
+                },
+                {
+                    "title": "Device Fingerprint",
+                    "dates" : "2011 - 2012",
+                    "description" : "Java-based systems development and maintenance to identify devices according to the information provided by its Browser. This information is used to recognize potential fraud, finding similar devices and classifing them according to their profile."
+                },
+                {
+                    "title": "Phishing Notification",
+                    "dates" : "2015 - 2016",
+                    "description" : "Java-based systems development and maintenance that identifies phishing based on the web site domain. The notification is done by a JavaScript that sends a request for servers stating the origin of phishing."
+                }
+            ]
+}
+
+
+projects.display = function(){
+    for(project in projects.projects){
+        $("#projects").append(HTMLprojectStart);
+
+        var formmatedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        $(".project-entry:last").append(formmatedTitle);
+
+        var formmatedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        $(".project-entry:last").append(formmatedDates);
+
+        var formmatedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+        $(".project-entry:last").append(formmatedDescription);
+    }
+}
+
+projects.display();
+
+if(bio.skills){
+    $("#header").append(HTMLheaderName.replace("%data%", "Renato Manzoni"));
+    $("#header").append(HTMLskillsStart);
+
+    $("#skills").append(HTMLskills.replace("%data%", bio.skills[0]));
+}
+
+function displayWork(){
+    for (index in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[index].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[index].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[index].dates);
+        $(".work-entry:last").append(formattedDates);
+
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[index].description);
+        $(".work-entry:last").append(formattedDescription);
+    }
+}
+
+displayWork();
+
+$(document).click(function(loc) {
+    logClicks(loc.pageX, loc.pageY);
+ });
+
+function inMain(name){
+    name = name.trim().split(" ");
+    name[1] = name[1].toUpperCase();
+    name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+    return name[0] + " " + name[1];
+}
+
+$('#main').append(internationalizeButton);
+
+$('#mapDiv').append(googleMap);
+
+
+
